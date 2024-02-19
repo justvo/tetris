0b14ea32f5a10c9fb14eb8770c85d92c8d5c19b7
justvo <vlad.otyan.02@gmail.com>
2024-02-19 21:17:19 +0200

    feat/create field

---
+++ script.js
@@ -0,0 +1,104 @@
+const PLAYFIELD_COLUMS = 10;
+const PLAYFIELD_ROWS = 20;
+const TETROMINO_NAMES = [
+    'O'
+]
+const TETROMINOES = {
+    'O': [
+        [1, 1],
+        [1, 1],
+    ]
+};
+let playField;
+let tetromino;
+
+function convertPositionToIndex(row, column) {
+    return row * PLAYFIELD_COLUMS + column
+}
+
+function generatePlayField() {
+    for (let i = 0; i < PLAYFIELD_ROWS * PLAYFIELD_COLUMS; i++) {
+        const div = document.createElement('div')
+        document.querySelector('.grid').append(div)
+    }
+    playField = new Array(PLAYFIELD_ROWS).fill()
+        .map(() => new Array(PLAYFIELD_COLUMS).fill(0))
+}
+
+function generateTetromino() {
+    const name = TETROMINO_NAMES[0];
+    const matrix = TETROMINOES[name];
+    tetromino = {
+        name,
+        matrix,
+        row: 1,
+        column: 3
+    }
+}
+
+generatePlayField();
+generateTetromino();
+
+const cells = document.querySelectorAll('.grid div')
+
+
+function drawPlayField() {
+    for (let row = 0; row < PLAYFIELD_ROWS; row++) {
+        for (let column = 0; column < PLAYFIELD_COLUMS; column++) {
+            drawTetromino();
+            drawPlayField();
+        }
+    }
+
+}
+
+function drawTetromino() {
+    const name = tetromino.name;
+    const tetrominoMatrixSize = tetromino.matrix.length;
+
+    for (let row = 0; row < tetrominoMatrixSize; row++) {
+        for (let column = 0; column < tetrominoMatrixSize; column++) {
+            const cellIndex = convertPositionToIndex(
+                tetromino.row + row,
+                tetromino.column + column
+            );
+            cells[cellIndex].classList.add(TETROMINO_NAMES)
+        }
+    }
+
+}
+// drawTetromino();
+// drawPlayField();
+
+function draw() {
+    cells.forEach(cell => cell.removeAttribute('class'))
+}
+// draw();
+
+document.addEventListener('keydown', onKeyDown)
+function onKeyDown(e) {
+    switch (e.key) {
+        case 'ArrowDown':
+            moveTetaminaDown();
+            break;
+        case 'ArrowDown':
+            moveTetaminaLeft();
+            break;
+        case 'ArrowDown':
+            moveTetaminaRight();
+            break;
+        case 'ArrowDown':
+            moveTetaminaDown();
+            break;
+    }
+    draw();
+}
+function moveTetaminaDown() {
+    tetromino.row++;
+}
+function moveTetaminaLeft() {
+    tetromino.column--;
+}
+function moveTetaminaRight() {
+    tetromino.column++;
+}

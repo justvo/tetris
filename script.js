0e66540c79918221de0f63a3d3927844fa0c5f24
Vlad <129364256+justvo@users.noreply.github.com>
2024-02-23 09:01:53 +0000

    feat/small adds

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ const TETROMINOES = {
         [1, 1],
     ],
     'J': [
-        [0, 1, 0],
-        [0, 1, 0],
-        [1, 1, 0],
+        [0, 0, 1],
+        [0, 0, 1],
+        [0, 1, 1],
     ],
     'L': [
         [0, 1, 0],
@@ -43,10 +43,10 @@ const TETROMINOES = {
         [0, 0, 0],
     ],
     'I': [
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
     ],
 };
 
@@ -86,8 +86,8 @@ function generateTetromino() {
     tetromino = {
         name,
         matrix,
-        row: 1 ,
-        column: Math.floor((PLAY_FIELD_COLUMS - matrix[0].length) / 2) ,
+        row: 0,
+        column: Math.floor((PLAY_FIELD_COLUMS - matrix[0].length) / 2),
     }
 }
 
@@ -143,21 +143,90 @@ function onKeyDown(e) {
         case 'ArrowRight':
             moveTetaminaRight();
             break;
+        case 'ArrowUp':
+            const rotatedMatrix = rotateTetromino(tetromino.matrix);
+
+            if (!checkColision(tetromino.row, tetromino.column, rotatedMatrix)) {
+                tetromino.matrix = rotatedMatrix;
+            }
+            break;
 
     }
     draw();
 }
 
 function moveTetaminaDown() {
-    tetromino.row++;
+    if (!checkColision(tetromino.row + 1, tetromino.column)) {
+        tetromino.row++;
+    }
 }
 
 function moveTetaminaLeft() {
-    tetromino.column--;
+    if (!checkColision(tetromino.row, tetromino.column - 1)) {
+        tetromino.column--;
+    }
 }
 
 function moveTetaminaRight() {
-    tetromino.column++;
+    if (!checkColision(tetromino.row, tetromino.column + 1)) {
+        tetromino.column++;
+    }
+}
+
+function rotateTetromino(matrix) {
+
+    const rotatedMatrix = [];
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        rotatedMatrix[i] = [];
+        for (let j = 0; j < matrix.length; j++) {
+            rotatedMatrix[i][j] = matrix[j][i];
+        }
+    }
+
+    for (let i = 0; i < rotatedMatrix.length; i++) {
+        rotatedMatrix[i].reverse();
+    }
+
+    return rotatedMatrix;
+}
+
+
+function removeZeros(matrix) {
+
+    let newMatrix =[];
+
+    matrix.forEach((row, rowIndex) => {
+        newMatrix[rowIndex] = row.filter(element => element !== 0);
+    });
+
+    newMatrix = newMatrix.filter(row => row.length > 0);
+
+    return newMatrix;
+}
+
+function longestIndexOfRow(matrix) {
+    const indexOfLongestRow = matrix.reduce((longestIndex, currentRow, currentIndex, array) => {
+        return currentRow.length > array[longestIndex].length ? currentIndex : longestIndex;
+    }, 0);
+    console.log(indexOfLongestRow)
+    return indexOfLongestRow;
+}
+
+
+function checkColision(newPositionRow, newPositionColumn, newMatrix = tetromino.matrix) {
+    const cleanedMatrix = removeZeros(newMatrix);
+
+    if (
+        newPositionRow < 0 ||
+        newPositionRow + cleanedMatrix.length > PLAY_FIELD_ROWS ||
+        newPositionColumn < 0 ||
+        newPositionColumn + cleanedMatrix[longestIndexOfRow(cleanedMatrix)].length > PLAY_FIELD_COLUMS
+    ) {
+        return true; 
+    }
+
+    return false; 
 }
 
 // main 
